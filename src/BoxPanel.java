2ac55c9935cc669cbcce9d0ea944d0c2eedf57f9
mikehopps <mhopps@gmail.com>
2017-10-02 13:20:27 -0400

    8x8 Grid with column 1 filled in.

--- src/BoxPanel.java
+++ src/BoxPanel.java
@@ -14,6 +14,22 @@ public class BoxPanel extends JPanel{
         Graphics2D g2 = (Graphics2D) g;
 
 
+        int width = getWidth()/8;
+
+        for(int j = 0; j < 8; j++) {
+            for (int i = 0; i < 8; i++) {
+
+                if(i == 1){
+                    g2.fillRect(i*width, j*width, width, width);
+                }
+                else {
+                    g2.drawRect(i * width, j * width, width, width);
+                }
+            }
+        }
+
+
+
     }
 
 }
