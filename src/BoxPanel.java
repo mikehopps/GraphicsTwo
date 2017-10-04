d61f9165bad2202cea94d338c334083fc0906f72
mikehopps <mhopps@gmail.com>
2017-10-04 12:48:46 -0400

    Didn't like the packaging - not easy to pull a single package.

--- src/BoxPanel.java
+++ src/BoxPanel.java
@@ -13,44 +13,19 @@ public class BoxPanel extends JPanel{
     public void paintComponent(Graphics g) {
         Graphics2D g2 = (Graphics2D) g;
 
-        //8 x 8 Random Grid!
-        //each cell has 50% chance of being a circle or square
-        //each cell has 50% chance of being filled or drawn
 
         int width = getWidth()/8;
         for(int j = 0; j < 8; j++) {
             for (int i = 0; i < 8; i++) {
-
-                double rand = Math.random();
-                if( rand < .25 ){
+                if( i % 2 == 1 || j % 2 == 0){
                     g2.fillRect(i*width, j*width, width, width);
-                }else if( rand < .5){
-                    g2.drawRect(i*width, j*width, width, width);
-
-                }else if( rand < .75){
-                    g2.drawOval(i*width, j*width, width, width);
-
-                }else{
-                    g2.fillOval(i*width, j*width, width, width);
                 }
-
+                else {
+                    g2.drawRect(i * width, j * width, width, width);
+                }
             }
         }
 
-
-        //Checkerboard:
-//        int width = getWidth()/8;
-//        for(int j = 0; j < 8; j++) {
-//            for (int i = 0; i < 8; i++) {
-//                if( (i + j) % 2 == 1 ){
-//                    g2.fillRect(i*width, j*width, width, width);
-//                }
-//                else {
-//                    g2.drawRect(i * width, j * width, width, width);
-//                }
-//            }
-//        }
-        //end Checkerboard
 
 
 
